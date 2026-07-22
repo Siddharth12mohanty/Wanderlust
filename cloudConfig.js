c07525c88e9ee8023818ddf1e179fe93d3587a44
Siddharth Mohanty <siddharthmohanty390@gmail.com>
2026-07-22 17:12:09 +0530

    Debug error handler

--- cloudConfig.js
+++ cloudConfig.js
@@ -10,10 +10,10 @@ cloudinary.config({
  
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: 'wanderlust_DEV',
-    allowerdFormats: ["png","jpg","jpeg"],
-  },
+ params: {
+    folder: "wanderlust_DEV",
+    allowedFormats: ["png","jpg","jpeg"],
+},
 });
 
 module.exports={
